14032d27f223062c40fc7628a69a310ab4f43d6b
Akash52 <ac8572611@gmail.com>
2021-02-08 09:50:40 +0530

    Form Component done

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React from 'react'
+import Footer from './components/Footer'
+import FormInput from './components/FormInput'
+import List from './components/List'
 
 export default function App() {
   return (
     <div className="App">
     <h1>To Do List</h1>
-      <form autoComplete="off">
-        <input type="text" name="todos" id="todos" required placeholder="What you do today !?"/>
-      <button type="submit">Create</button>
-      </form>
-      <ul>
-        <li>
-          <label htmlFor="">
-            <input type="checkbox" id=""/>
-            Cooking to do
-          </label>
-          <button>Edit</button>
-        </li>
-        <li>
-          <label htmlFor="">
-            <input type="checkbox" id=""/>
-            Study 2 hour
-          </label>
-          <button>Edit</button>
-        </li>
-      </ul>
-
-      <div className="row">
-      <label htmlFor="all">
-        <input type="checkbox" name="all" id="all"/>
-        All
-      </label>
-      <p>You have 0 to do</p>
-    <button id="delete">Delete</button>
-      </div>
-
+      <FormInput/>
+      <List/>
+      <Footer/>
     </div>
   )
 }
