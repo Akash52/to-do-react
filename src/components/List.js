338cf43ca499b3e1fcfc43d23338c0f1b9559ce2
Akash52 <ac8572611@gmail.com>
2021-02-16 20:13:42 +0530

    Chechked or Not

--- src/components/List.js
+++ src/components/List.js
@@ -5,12 +5,18 @@ import { DataContext } from './DataProvider'
 export default function List() {
   const [todos, setTodos] = useContext(DataContext)
 
-  console.log(todos)
-
+  const switchComplete = (id) => {
+    alert(id)
+  }
   return (
     <ul>
       {todos.map((todo, index) => (
-        <ListItem todo={todo} key={index} id={index} />
+        <ListItem
+          todo={todo}
+          key={index}
+          id={index}
+          checkComplete={switchComplete}
+        />
       ))}
     </ul>
   )
