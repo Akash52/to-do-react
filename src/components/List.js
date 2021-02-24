9f743c3ad9206a3345b4b18537a3745588c919a8
Akash52 <ac8572611@gmail.com>
2021-02-24 17:40:34 +0530

    swithcomplete added

--- src/components/List.js
+++ src/components/List.js
@@ -4,9 +4,15 @@ import { DataContext } from './DataProvider'
 
 export default function List() {
   const [todos, setTodos] = useContext(DataContext)
-
+  // todo Either complete or not
   const switchComplete = (id) => {
-    alert(id)
+    const newTodos = [...todos]
+    newTodos.forEach((todo, index) => {
+      if (index === id) {
+        todo.complete = !todo.complete
+      }
+    })
+    setTodos(newTodos)
   }
   return (
     <ul>
